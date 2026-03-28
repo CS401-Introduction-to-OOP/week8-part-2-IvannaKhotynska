4eae27f0d9be5528570d163ce97c683e1a71cc97
IvannaKhotynska <ivanna.khotynska.25@kse.org.ua>
2026-03-28 16:49:28 +0200

    stage2

---
+++ Task2/BoxingTester.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;
+        object b = a;
+        int c = (int)b;
+        
+        Console.WriteLine($"a - {a}");
+        Console.WriteLine($"b - {b}");
+        Console.WriteLine($"c - {c}");
+        
+        /* тут зробили boxing a - і записали в b
+         а у змінній с - unboxing b
+         */
+        
+    }
+}
