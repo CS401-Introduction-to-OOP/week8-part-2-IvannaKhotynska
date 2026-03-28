78a553bb669bd851f781436d77d53709508743ad
IvannaKhotynska <ivanna.khotynska.25@kse.org.ua>
2026-03-28 16:58:23 +0200

    stage3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Task2;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point { X = 1, Y = 2 };
+        Point p2 = new Point { X = 3, Y = 4 };
+        p1.Print();
+        p2.Print();
+        
+        PointRef r1 = new PointRef { X = 1, Y = 2 };
+        PointRef r2 = new PointRef { X = 7, Y = 8 };
+        r1.Print();
+        r2.Print();
+        
+        BoxingTester.Test();
+    }
+}    
