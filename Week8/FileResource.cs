04efeee42befeeee17f8ea535d61ed23dbdffde6
IvannaKhotynska <ivanna.khotynska.25@kse.org.ua>
2026-03-28 15:57:53 +0200

    stage1

---
+++ Week8/FileResource.cs
@@ -0,0 +1,22 @@
+﻿namespace Week8;
+
+public class FileResource : Resource, IDisposable
+{
+    public FileResource(string name) : base(name)
+    {
+    }
+    public override void Open()
+    {
+        bool IsOpen = true;
+    }
+    public override void Close()
+    {
+        Dispose();
+    }
+    public void Dispose()
+    {
+        Close();
+        Console.WriteLine("The dispose was called");
+    }
+
+}
---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,22 @@
+﻿namespace Week8;
+
+public class NetworkResource : Resource, IDisposable
+{
+    public NetworkResource(string name) : base(name)
+    {
+    }
+    public override void Open()
+    {
+        bool IsOpen = true;
+    }
+    public override void Close()
+    {
+        Dispose();
+    }
+    public void Dispose()
+    {
+        Close();
+        Console.WriteLine("The dispose was called");
+    }
+
+}
