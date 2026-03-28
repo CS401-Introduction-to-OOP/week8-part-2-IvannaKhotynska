767ebf72970f556f8cc09afecd72a36aac10633c
IvannaKhotynska <ivanna.khotynska.25@kse.org.ua>
2026-03-28 17:11:19 +0200

    stage2

---
+++ Task3/DataContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace Task3;
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    public long GetTotalSize()
+    {
+        long totalSize = 0;
+        foreach (var item in _items)
+        {
+            totalSize += item.GetSize();
+        }
+
+        return totalSize;
+    }
+}
